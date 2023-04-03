cd507148f5e6f3d4e7ec3179d00b13bc85a829c9
WB_777 <antariuzz@gmail.com>
2023-04-03 17:28:23 +0300

    Initial commit

---
+++ src/main/java/lab/space/vilki_palki/VilkiPalkiApplication.java
@@ -0,0 +1,13 @@
+package com.example.vilki_palki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VilkiPalkiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VilkiPalkiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lab/space/vilki_palki/VilkiPalkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vilki_palki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VilkiPalkiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
