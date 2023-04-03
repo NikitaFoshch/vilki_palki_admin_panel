9dc1584f70628b09b1d29e954fc197dc090fe1a5
WB_777 <antariuzz@gmail.com>
2023-04-03 20:12:43 +0300

    Add MvcConfig

--- src/main/java/lab/space/vilki_palki/config/MvcConfig.java
+++ src/main/java/lab/space/vilki_palki/config/MvcConfig.java
@@ -1,4 +1,17 @@
 package lab.space.vilki_palki.config;
 
-public class MvcConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Paths;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        //Добавление пути к загруженным фото
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("file:///" + Paths.get("files").toFile().getAbsolutePath() + "/");
+    }
 }
